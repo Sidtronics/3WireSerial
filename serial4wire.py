15ef1975af7d93c9c9c0f03dd1fc041a276529c3
Siddhesh Dharme <siddheshdharme18@gmail.com>
2023-08-24 17:29:41 +0530

    Renamed: 4WireSerial.py -> serial4wire.py

--- serial4wire.py
+++ serial4wire.py
@@ -12,10 +12,6 @@ class Command(Enum):
     STB_HIGH = 'H'
     STB_LOW = 'L'
 
-# defs for set_stb().
-HIGH = True;
-LOW = False;
-
 class SerialDevice :
 
     def __init__(self, port) :
@@ -34,27 +30,16 @@ class SerialDevice :
                 print("[4WireSerial] Port opened at:", self.ser.name)
                 break
 
-    def send(self, *data:int, count:int=1) :
+    # Low level methods:
+
+    def send(self, *data:int) :
 
         self.ser.write(Command.SEND.value.encode())
-        self.ser.write((len(data)*count).to_bytes(1))
-
-        for _ in range(count) :
-            self.ser.write(bytes(data))
+        self.ser.write(len(data).to_bytes(1))
+        self.ser.write(bytes(data))
 
         sent_bytes = self.ser.read()
         return int.from_bytes(sent_bytes)
-
-        
-    def send_str(self, string:str) :
-
-        self.ser.write(Command.SEND.value.encode())
-        self.ser.write(len(string).to_bytes(1))
-        self.ser.write(string.encode())
-
-        sent_bytes = self.ser.read()
-        return int.from_bytes(sent_bytes)
-
         
     def recv(self, count:int=1) :
         
@@ -62,38 +47,49 @@ class SerialDevice :
         self.ser.write(count.to_bytes(1))
         
         received_bytes = list(self.ser.read(count))
-                
         return received_bytes
         
-    def set_stb(self, val:bool) :
+    def stb(self, val:bool) :
 
         cmd = Command.STB_HIGH if val else Command.STB_LOW
 
         self.ser.write(cmd.value.encode())
         self.ser.read() # ACK
+
+    # Other convenient methods:
+
+    def send_str(self, string:str) :
+        
+        sent_bytes = self.send(*tuple(map(ord, string)))
+        return sent_bytes
+
+    def send_cmd(self, cmd:int, *data:int) :
+
+        self.stb(False)
+        sent_bytes = self.send(cmd)
+
+        if data != () :
+            sent_bytes += self.send(*data)
+
+        self.stb(True)
+
+        return sent_bytes 
         
 if __name__ == "__main__":
 
 
     dbg = SerialDevice(PORT)
 
-    def send(*data:int, count:int=1) :
+    # Wrapper functions for verbosity and convenience:
+
+    def send(*data:int) :
 
         print("\n     HEX  BIN        DEC")
-        for n in range(count*len(data)) :
-            print("[{0:02d}] {1:02X} : {1:08b} : {1:03d}".format(n+1, data[n % len(data)]))
+        for n in range(len(data)) :
+            print("[{0:02d}] {1:02X} : {1:08b} : {1:03d}".format(n+1, data[n]))
 
-        sent = dbg.send(*data, count=count)
+        sent = dbg.send(*data)
         print(f"\n{sent} Byte(s) sent.\n")
-
-    def send_str(string:str) :
-        
-        print("\n     HEX  BIN        DEC   ASCII")
-        for n in range(len(string)) :
-            print("[{0:02d}] {1:02X} : {1:08b} : {1:03d} : {1:c}".format(n+1, ord(string[n])))
-
-        sent = dbg.send_str(string)
-        print(f"\n{sent} Character(s) sent.\n")
 
     def recv(count:int=1) :
 
@@ -105,10 +101,30 @@ if __name__ == "__main__":
 
         print(f"\n{count} Byte(s) received.\n")
 
-    def set_stb(val:bool) :
+    def stb(val:bool) :
 
-        dbg.set_stb(val)
+        dbg.stb(val)
         print("\nStrobe pin held {}.\n".format("HIGH" if val else "LOW"))
+
+    def send_str(string:str) :
+        
+        print("\n     HEX  BIN        DEC   ASCII")
+        for n in range(len(string)) :
+            print("[{0:02d}] {1:02X} : {1:08b} : {1:03d} : {1:c}".format(n+1, ord(string[n])))
+
+        sent = dbg.send_str(string)
+        print(f"\n{sent} Character(s) sent.\n")
+
+    def send_cmd(cmd:int, *data:int) :
+
+        print("\n      HEX  BIN        DEC")
+        print("[CMD] {0:02X} : {0:08b} : {0:03d}".format(cmd))
+        for n in range(len(data)) :
+            print("[{0:03d}] {1:02X} : {1:08b} : {1:03d}".format(n+1, data[n]))
+
+        sent = dbg.send_cmd(cmd, *data)
+        print(f"\n{sent} Byte(s) sent.\n")
+
 
     # Start interactive console.
     print("[4WireSerial] Starting interactive console:\n")
